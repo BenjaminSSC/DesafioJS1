7bfd5c296e23e27832072610c50a836db263ed4d
BenjaminSSC <abssc1514@gmail.com>
2024-06-09 19:19:09 -0400

    projecto

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+const precio = 400000;
+let cantidad = 0; 
+let valorTotal = 0;
+
+
+precioSpan = document.querySelector(".precio-inicial");
+precioSpan.innerHTML = precio;
+
+cantidadSpan = document.querySelector('.cantidad');
+
+totalSpan = document.querySelector('.valor-total');
+
+
+
+
